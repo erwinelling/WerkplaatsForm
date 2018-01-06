7578057d0eb4cb6dcec4a31ba5e8562fe8f36cc8
Erwin Elling <erwinelling@Erwins-MacBook-Pro.local>
2018-01-06 16:36:13 +0100

    Changed some paths

--- form.js
+++ form.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
   $("#form").alpaca({
-      "schemaSource": "/data/werkplaats-schema.json",
-      "optionsSource": "/data/werkplaats-options.json",
-      "viewSource": "/data/werkplaats-view.json",
-      "view": {
-        "wizard": {
-          "buttons": {
-            "submit": {
-              "click": function(e) {
-                  alert(JSON.stringify(this.getValue(), null, "  "));
-              }
-            }
-          }
-        }
-      }
+      "schemaSource": "http://localhost/~erwinelling/Werkplaats/WerkplaatsForm/data/werkplaats-schema.json",
+      "optionsSource": "http://localhost/~erwinelling/Werkplaats/WerkplaatsForm/data/werkplaats-options.json",
+      "viewSource": "http://localhost/~erwinelling/Werkplaats/WerkplaatsForm/data/werkplaats-view.json",
+      // "view": {
+      //   "wizard": {
+      //     "buttons": {
+      //       "submit": {
+      //         "click": function(e) {
+      //             alert(JSON.stringify(this.getValue(), null, "  "));
+      //         }
+      //       }
+      //     }
+      //   }
+      //}
   });
 });
